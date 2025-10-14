a0805ca6145303efc7f2ef4b8147f206dee575e1
Zama <zamabrightness720@gmail.com>
2025-10-14 11:23:23 -0700

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,20 @@
+number1 = 10
+number2 = 5
+
+addition = 10 + 5
+
+print("Addition of", number1, "and", number2, "is", addition)
+
+number1 = 10
+number2 = 5
+
+subtraction = 10 - 5
+
+print("subtraction of", number1, "and", number2, "is", subtraction)
+
+number1 = 10
+number2 = 5
+
+multiplication= 10*5
+
+print("Multiplication of", number1, "and", number2, "is", multiplication)
